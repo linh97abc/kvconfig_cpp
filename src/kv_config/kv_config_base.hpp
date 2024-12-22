bd57b22ef756f8f37b012ac32581743fd7f08a78
Tuấn Linh Bùi <linh97abc@gmail.com>
2024-12-23 00:16:38 +0700

    Enhance KV_Config_Base and IPAddress classes with detailed documentation for configuration locking, unlocking, and IP address handling methods.

--- src/kv_config/kv_config_base.hpp
+++ src/kv_config/kv_config_base.hpp
@@ -7,7 +7,6 @@
 #include <cstdint>
 #include <iostream>
 
-
 /**
  * @class KV_Config_Base
  * @brief A base class for key-value configuration management.
@@ -24,7 +23,22 @@
 class KV_Config_Base
 {
 public:
+
+
+    /**
+     * @brief Locks the configuration to prevent concurrent modifications.
+     * 
+     * This method is intended to be overridden by derived classes to implement
+     * specific locking mechanisms. The default implementation does nothing.
+     */
     virtual void Lock() {}
+
+    /**
+     * @brief Unlocks the configuration to allow modifications.
+     * 
+     * This method is intended to be overridden by derived classes to implement
+     * specific unlocking mechanisms. The default implementation does nothing.
+     */
     virtual void UnLock() {}
 
     template <typename T>
@@ -42,6 +56,13 @@ public:
         return !stream.fail() && stream.eof();
     }
 
+    /**
+     * @brief Returns the configuration data as a string.
+     *
+     * This function returns the configuration data as a string.
+     *
+     * @return const std::string& The configuration data as a string.
+     */
     const std::string &str() const
     {
         return this->__config_str;
@@ -113,7 +134,7 @@ private:
 /**
  * @class IPAddress
  * @brief A class to represent and manipulate IP addresses.
- * 
+ *
  * This class provides functionalities to parse, encode, and validate IP addresses.
  * It supports initialization from strings, character arrays, and byte arrays.
  */
@@ -131,6 +152,14 @@ class IPAddress
     void encode();
 
 public:
+    /**
+     * @brief Default constructor for the IPAddress class.
+     * 
+     * This constructor initializes the IPAddress object with default values:
+     * - The address is set to all zeros.
+     * - The mask is set to 0.
+     * - The validity flag is set to false.
+     */
     IPAddress()
     {
         this->addr.fill(0);
@@ -138,16 +167,33 @@ public:
         this->is_valid = false;
     }
 
+    /**
+     * @brief Constructs an IPAddress object and parses the given IP address string.
+     * 
+     * @param ipAddr A C-style string representing the IP address to be parsed.
+     */
     IPAddress(const char *ipAddr)
     {
         this->parse(ipAddr);
     }
 
+
+    /**
+     * @brief Constructs an IPAddress object and parses the given IP address string.
+     * 
+     * @param ipAddr A std::string representing the IP address to be parsed.
+     */
     IPAddress(const std::string &ipAddr)
     {
         this->parse(ipAddr);
     }
 
+    /**
+     * @brief Constructs an IPAddress object with the given address and optional mask.
+     * 
+     * @param addr A std::array of 4 uint8_t elements representing the IP address.
+     * @param mask An optional uint8_t value representing the subnet mask. Default is 0.
+     */
     IPAddress(const std::array<uint8_t, 4> &addr, uint8_t mask = 0)
     {
         this->addr = addr;
@@ -155,6 +201,14 @@ public:
         this->encode();
     }
 
+    /**
+     * @brief Copy constructor for the IPAddress class.
+     *
+     * This constructor creates a new IPAddress object by copying the values
+     * from an existing IPAddress object.
+     *
+     * @param ip The IPAddress object to copy from.
+     */
     IPAddress(const IPAddress &ip)
     {
         this->ip = ip.ip;
@@ -162,29 +216,73 @@ public:
         this->mask = ip.mask;
     }
 
+    /**
+     * @brief Checks if the data is valid.
+     *
+     * @return true if the data is valid, false otherwise.
+     */
     bool IsDataValid() const
     {
         return this->is_valid;
     }
 
+    /**
+     * @brief Returns the IP address as a string.
+     * 
+     * This function provides access to the IP address stored in the object.
+     * 
+     * @return const std::string& A reference to the IP address string.
+     */
     const std::string &str() const
     {
         return this->ip;
     }
 
+    /**
+     * @brief Get the address as a constant reference to a std::array of 4 uint8_t elements.
+     *
+     * @return const std::array<uint8_t, 4>& Reference to the address array.
+     */
     const std::array<uint8_t, 4> &address() const
     {
         return this->addr;
     }
 
+    /**
+     * @brief Retrieves the subnet mask.
+     *
+     * This function returns the subnet mask. If the mask is not set (i.e., it is zero),
+     * it defaults to returning 24.
+     *
+     * @return uint8_t The subnet mask, or 24 if the mask is not set.
+     */
     uint8_t subnet() const
     {
         return this->mask ? this->mask : 24;
     }
 
+    /**
+     * @brief Calculates the subnet mask based on the subnet number.
+     *
+     * This function computes the subnet mask for the IP address by shifting bits
+     * according to the subnet number. The subnet mask is returned as an array of
+     * four 8-bit unsigned integers.
+     *
+     * @return std::array<uint8_t, 4> The subnet mask as an array of four bytes.
+     */
     std::array<uint8_t, 4> sn_mask() const;
 };
 
+/**
+ * @brief Overloaded output stream operator for the IPAddress class.
+ *
+ * This function overloads the output stream operator to allow printing
+ * IPAddress objects to an output stream.
+ *
+ * @param os The output stream to write to.
+ * @param ip The IPAddress object to write.
+ * @return std::ostream& A reference to the output stream.
+ */
 std::ostream &operator<<(std::ostream &os, const IPAddress &ip);
 
 #endif // KV_CONFIG_BASE_HPP
